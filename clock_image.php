6a67a84340b072dc63fa7f460d15aac8453e8bb2
Kevin Le Brun <lebrun.k@gmail.com>
2012-03-23 11:49:36 +0100

    First commit

---
+++ clock_image.php
@@ -0,0 +1,32 @@
+<?php
+
+$format = isset($_GET['format']) ? $_GET['format'] : false;
+
+function create_image_clock()
+{
+    $width = 150;
+    $height = 40;
+    $img = imagecreatetruecolor($width, $height);
+    $white = imagecolorallocate($img, 255, 255, 255);
+    $black = imagecolorallocate($img, 0, 0, 0);
+    $date = date('H:i:s');
+    imagefilledrectangle($img, 0, 0, $width, $height, $white);
+    $font = './PontanoSans-Regular.ttf';
+    imagettftext($img, 24, 0, 5, 32, $black, $font, $date);
+    return $img;
+}
+
+$img = create_image_clock();
+
+if ($format === 'png') {
+    header('Content-type: image/png');
+    imagepng($img);
+} else if ($format === 'gif') {
+    header('Content-type: image/gif');
+    imagegif($img);
+} else {
+    header('Content-type: image/jpeg');
+    imagejpeg($img);
+}
+
+imagedestroy($img);
